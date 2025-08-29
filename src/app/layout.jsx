1d1159a8f81f86a921fcc4c89410125c5f19d7ba
Michael Chang <michaelchang@alumni.usc.edu>
2025-08-29 09:50:19 -0500

    about page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,21 @@ import FrameNav from "./components/FrameNav";
 import SiteChrome from "./components/SiteChrome";
 import "./globals.css";  // ✅ global styles here
 
+import { Cormorant_Garamond } from "next/font/google";
+import { Cormorant_Unicase } from "next/font/google";
+
+const cormorantUni = Cormorant_Unicase({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
+
+const cormorant = Cormorant_Garamond({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
 export const metadata = {
   title: "Derek Calkins Tattoo Portfolio",
   description: "Portfolio of tattoo artist Derek Calkins",
@@ -11,7 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cormorant.className}>
       <body>
          <SiteChrome>{children}</SiteChrome>
       </body>
